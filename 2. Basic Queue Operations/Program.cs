4bc13193cd7f30587bdf109efe7a6e46204bccc6
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-09-15 20:57:38 +0300

    Add files via upload

---
+++ 2. Basic Queue Operations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Channels;
+
+int[] commands = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int queueLength = commands[0];
+int queueToPop = commands[1];
+int queueToCheck = commands[2];
+
+Queue<int>queue = new Queue<int>(values.Take(queueLength));
+
+while (queue.Any() && queueToPop > 0) 
+{
+    queue.Dequeue();
+    queueToPop--;
+}
+if (queue.Contains(queueToCheck))
+{
+    Console.WriteLine("true");
+}
+else if (queue.Count == 0)
+{
+    Console.WriteLine("0");
+}
+else 
+{
+    Console.WriteLine(queue.Min());
+}
+
