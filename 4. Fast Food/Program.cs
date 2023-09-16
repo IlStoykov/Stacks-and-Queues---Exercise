645c5cd4cef267d7704036a062512e5b2ae9f686
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-09-16 16:19:54 +0300

    Add files via upload

---
+++ 4. Fast Food/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int food = int.Parse(Console.ReadLine());
+int[] entr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> orders = new Queue<int>(entr);
+Console.WriteLine(orders.Max());
+
+while (orders.Count > 0 && food > 0) 
+{    
+    int order_check = orders.Peek();
+
+    if (food >= order_check)
+    {
+        orders.Dequeue();
+        food -= order_check;
+    }
+    else 
+    {
+        break;
+    }
+    
+}
+if (orders.Count == 0)
+{
+    Console.WriteLine("Orders complete");
+}
+else 
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+}
+
+
+
