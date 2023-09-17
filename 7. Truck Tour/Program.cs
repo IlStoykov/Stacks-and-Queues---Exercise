5984b5e019a90fe043e523869cad772d7010b7cd
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2023-09-17 11:38:33 +0300

    Add files via upload

---
+++ 7. Truck Tour/Program.cs
@@ -0,0 +1,33 @@
+﻿int rounds = int.Parse(Console.ReadLine());
+Queue<int[]> pumps = new Queue<int[]>();
+for (int i = 0; i < rounds; i++) 
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    pumps.Enqueue(input);
+}
+int bestRoud = 0;
+while (true) 
+{
+    int totalPetrol = 0;
+    foreach (int[] pump in pumps) 
+    {
+        totalPetrol += pump[0];
+        int curentPath = pump[1];
+        if (totalPetrol < curentPath)
+        {
+            totalPetrol = 0;
+            break;            
+        }
+        else 
+        {
+            totalPetrol -= curentPath;
+        }
+    }
+    if (totalPetrol > 0) 
+    {
+        break;
+    }
+    bestRoud++;
+    pumps.Enqueue(pumps.Dequeue());
+}
+Console.WriteLine(bestRoud);
